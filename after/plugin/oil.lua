4b403696991ed02d6fdc843327a39fd0a8d6c3f2
kyle <ona@lunch.run>
2023-05-22 10:04:15 -0500

    Turn off the default preview action in oil

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -1,3 +1,21 @@
-require("oil").setup()
+require("oil").setup({
+	columns = {},
+	keymaps = {
+			["g?"] = "actions.show_help",
+			["<CR>"] = "actions.select",
+			["<C-s>"] = "actions.select_vsplit",
+			["<C-h>"] = "actions.select_split",
+			["<C-t>"] = "actions.select_tab",
+			["<C-c>"] = "actions.close",
+			["<C-l>"] = "actions.refresh",
+			["-"] = "actions.parent",
+			["_"] = "actions.open_cwd",
+			["`"] = "actions.cd",
+			["~"] = "actions.tcd",
+			["g."] = "actions.toggle_hidden",
+	},
+  use_default_keymaps = false,
+
+})
 
 vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
